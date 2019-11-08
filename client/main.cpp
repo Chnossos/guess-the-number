ead53f09d90d79297ab3aca6170cd28c17be625c
Pierre Caissial <pierre.caissial@vivoka.com>
2019-11-08 10:37:19 +0100

    Projects created + .gitignore added

---
+++ client/main.cpp
@@ -0,0 +1,17 @@
+/*
+** Copyright 2019 Vivoka
+**
+** Made by Pierre Caissial on 2019/11/8
+** Mail <pierre.caissial@vivoka.com>
+**
+** vivoka.com
+*/
+
+#include <QCoreApplication>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication app { argc, argv };
+
+    return app.exec();
+}
